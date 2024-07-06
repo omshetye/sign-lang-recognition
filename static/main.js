51bbfafcd9be86135c02dd183f3317831d41141c
omshetye <103515277+omshetye@users.noreply.github.com>
2024-07-06 10:12:26 +0530

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,162 +1,147 @@
-from flask import Flask, render_template, request, jsonify
-import base64
-import numpy as np
-import mediapipe as mp
-import joblib
-import cv2
-import pyttsx3
-import threading
+const video = document.getElementById('videoElement');
+let stream = null;
 
-app = Flask(__name__)
-camera_enabled = False
+function startCamera() {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(function(usrStream) {
+                stream = usrStream;
+                video.srcObject = stream;
+                video.play();
+                captureFrame();
+            })
+            .catch(function(err) {
+                console.error('Error accessing the camera:', err);
+            });
+    } else {
+        console.error('getUserMedia is not supported by this browser');
+    }
+}
 
-# Load the model
-model_path = 'model/model_2.p'
-model_dict = joblib.load(model_path)
-model = model_dict['model']
+function sendTextToTts() {
+    const text = document.getElementById('pred-sentence').value
+    const speakButton = document.getElementById('speak-button');
+    speakButton.disabled = true;
+    axios.post('/speak', { text: text })
+        .then(response => {
+            // Log the response if needed
+            console.log(response.data.message);
+        })
+        .catch(error => {
+            // Handle errors if needed
+            console.error('Error sending text for TTS:', error);
+        });
 
-# Initialize MediaPipe Hands
-mp_hands = mp.solutions.hands
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+    // Re-enable the button after 2 seconds
+    setTimeout(() => {
+        speakButton.disabled = false;
+        ttsStatus.innerText = '';
+    }, 2000);
+}
 
-@app.route('/camera', methods=['POST'])
-def toggle_camera():
-    global camera_enabled
-    camera_enabled = not camera_enabled
-    return jsonify({'camera_enabled': camera_enabled})
+function toggleCamera() {
+    if (!stream) {
+        startCamera();
+        document.getElementById('camera-button').innerHTML = '<i class="fas fa-camera-slash"></i> Disable Camera';
+    } else {
+        stopCamera();
+        document.getElementById('camera-button').innerHTML = '<i class="fas fa-camera"></i> Enable Camera';
+    }
+}
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+function stopCamera() {
+    if (stream) {
+        stream.getTracks().forEach(track => track.stop());
+        video.srcObject = null;
+        stream = null;
+    }
+}
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    data = request.json
-    frame_data = data.get('frame', '')
+function captureFrame() {
+    const canvas = document.createElement('canvas');
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    const context = canvas.getContext('2d');
+    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    const imageDataURL = canvas.toDataURL('image/jpeg');
+    sendFrame(imageDataURL);
+
+    setTimeout(captureFrame, 2000); // Adjust frame capture interval as needed
+}
+
+function sendFrame(imageDataURL) {
+    axios.post('/predict', { frame: imageDataURL })
+        .then(response => {
+            const prediction = response.data.prediction;
+            if (prediction === 'space') {
+                document.getElementById('prediction').innerText = 'Current prediction: ' + prediction;
+                addSpace();
+            } else if (prediction === 'back') {
+                document.getElementById('prediction').innerText = 'Current prediction: ' + prediction;
+                removeLastCharacter();
+            } else {
+                document.getElementById('prediction').innerText = 'Current prediction: ' + prediction;
+                document.getElementById('pred-sentence').value += prediction;
+            }
+        })
+        .catch(error => {
+            console.error('Error getting prediction:', error);
+            document.getElementById('prediction').innerText = 'Error fetching prediction';
+        });
+}
+
+function removeLastCharacter() {
+    var predSentenceElement = document.getElementById('pred-sentence');
+    var textContent = predSentenceElement.value;
     
-    if frame_data:
-        # Decode base64 image data
-        frame_bytes = base64.b64decode(frame_data.split(',')[1])
-        # Convert to numpy array
-        frame_np = np.frombuffer(frame_bytes, dtype=np.uint8)
-        # Decode image
-        if frame_np is not None:
-            frame = cv2.imdecode(frame_np, flags=cv2.IMREAD_COLOR)
-            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            results = hands.process(frame_rgb)
-            
-            data_aux = []
-            x_ = []
-            y_ = []
+    if (textContent.length > 0) {
+        predSentenceElement.value = textContent.slice(0, -1);
+    }
+}
 
-            if results.multi_hand_landmarks:
-                for hand_landmarks in results.multi_hand_landmarks:
-                    for i in range(len(hand_landmarks.landmark)):
-                        x = hand_landmarks.landmark[i].x
-                        y = hand_landmarks.landmark[i].y
-                        x_.append(x)
-                        y_.append(y)
+function clearWord() {
+    document.getElementById('pred-sentence').value = '';
+}
 
-                if len(x_) > 0 and len(y_) > 0:
-                    min_x = min(x_)
-                    min_y = min(y_)
-                    
-                    for i in range(len(hand_landmarks.landmark)):
-                        x = hand_landmarks.landmark[i].x
-                        y = hand_landmarks.landmark[i].y
-                        data_aux.append(x - min_x)
-                        data_aux.append(y - min_y)
+const spaceChar = "  ";
 
-                    if len(data_aux) > 0:
-                        prediction = model.predict([np.asarray(data_aux)])
-                        predicted_character = prediction[0]
-                        print(predicted_character)
-                        return jsonify(prediction=predicted_character)
-                    else:
-                        return jsonify(prediction="No valid hand landmarks detected")
-                else:
-                    return jsonify(prediction="No valid hand landmarks detected")
-            else:
-                return jsonify(prediction="")
-        else:
-            return jsonify(prediction="Frame data not received")
+function addSpace() {
+    var predSentenceElement = document.getElementById('pred-sentence');
+    if (predSentenceElement.value.length > 0) {
+        predSentenceElement.value += `${spaceChar}`;
+    }
+}
 
-@app.route('/stream_video', methods=['POST'])
-def stream_video():
-    data = request.json
-    frame_data = data.get('frame', '')
-    
-    if frame_data:
-        # Decode base64 image data
-        frame_bytes = base64.b64decode(frame_data.split(',')[1])
-        # Convert to numpy array
-        frame_np = np.frombuffer(frame_bytes, dtype=np.uint8)
-        # Decode image
-        frame = cv2.imdecode(frame_np, flags=cv2.IMREAD_COLOR)
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        results = hands.process(frame_rgb)
-        
-        data_aux = []
-        x_ = []
-        y_ = []
+document.addEventListener('DOMContentLoaded', () => {
+    axios.get('/camera')
+        .then(response => {
+            if (response.data.camera_enabled) {
+                startCamera();
+                document.getElementById('camera-button').innerHTML = '<i class="fas fa-camera-slash"></i> Disable Camera';
+            } else {
+                stopCamera();
+                document.getElementById('camera-button').innerHTML = '<i class="fas fa-camera"></i> Enable Camera';
+            }
+        })
+        .catch(error => console.error('Error fetching initial camera state:', error));
+});
 
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                for i in range(len(hand_landmarks.landmark)):
-                    x = hand_landmarks.landmark[i].x
-                    y = hand_landmarks.landmark[i].y
-                    x_.append(x)
-                    y_.append(y)
-
-            if len(x_) > 0 and len(y_) > 0:
-                min_x = min(x_)
-                min_y = min(y_)
-                
-                for i in range(len(hand_landmarks.landmark)):
-                    x = hand_landmarks.landmark[i].x
-                    y = hand_landmarks.landmark[i].y
-                    data_aux.append(x - min_x)
-                    data_aux.append(y - min_y)
-
-                if len(data_aux) > 0:
-                    prediction = model.predict([np.asarray(data_aux)])
-                    predicted_character = prediction[0]
-                    return jsonify(prediction=predicted_character)
-                else:
-                    return jsonify(prediction="No valid hand landmarks detected")
-            else:
-                return jsonify(prediction="No valid hand landmarks detected")
-        else:
-            return jsonify(prediction="")
-    else:
-        return jsonify(prediction="Frame data not received")
-    
-def speak_text(text):
-    # Reinitialize the pyttsx3 engine within the thread
-    engine = pyttsx3.init()
-    engine.setProperty('rate', 150)  # Adjust speaking rate (default is 200)
-    # Change voice (optional)
-    voices = engine.getProperty('voices')
-    engine.setProperty('voice', voices[1].id)
-    engine.say(text)
-    engine.runAndWait()
-
-
-@app.route('/speak', methods=['POST'])
-def speak():
-    data = request.json
-    text = data.get('text', '')
+document.addEventListener('DOMContentLoaded', function() {
+    const checkbox = document.getElementById('dark-mode-checkbox');
   
-    if text:
-        # Run the text-to-speech in a separate thread
-        tts_thread = threading.Thread(target=speak_text, args=(text,))
-        tts_thread.start()
-        return jsonify({'status': 'success', 'message': 'Text has been spoken'}), 200
-    else:
-        return jsonify({'status': 'error', 'message': 'No text provided'}), 400
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    if (localStorage.getItem('dark-mode') === 'enabled') {
+      document.body.classList.add('dark-mode');
+      checkbox.checked = true;
+    }
+  
+    checkbox.addEventListener('change', function() {
+      if (checkbox.checked) {
+        document.body.classList.add('dark-mode');
+        localStorage.setItem('dark-mode', 'enabled');
+      } else {
+        document.body.classList.remove('dark-mode');
+        localStorage.setItem('dark-mode', 'disabled');
+      }
+    });
+});
